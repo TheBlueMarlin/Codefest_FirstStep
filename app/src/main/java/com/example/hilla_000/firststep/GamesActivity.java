615f29fa332ce4d05e7b0c402bb12545a72b0ba1
Quinn Roundy <qjr3@students.uwf.edu>
2016-04-02 01:04:15 -0500

    Removed Fragments Added Games, Lessons, and Tests Activity Starting Menu

--- app/src/main/java/com/example/hilla_000/firststep/GamesActivity.java
+++ app/src/main/java/com/example/hilla_000/firststep/GamesActivity.java
@@ -1,15 +1,21 @@
 package com.example.hilla_000.firststep;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
-public class MainActivity extends MasterActivity {
+public class GamesActivity extends MasterActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_games);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
     }
+
 }
--- app/src/main/java/com/example/hilla_000/firststep/MenuActivity.java
+++ app/src/main/java/com/example/hilla_000/firststep/MenuActivity.java
@@ -1,10 +1,11 @@
 package com.example.hilla_000.firststep;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,8 +16,20 @@ public class MenuActivity extends MasterActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    public void btn_goto_lesson_onClick(View view) {
+        startActivity(new Intent(getApplicationContext(), LessonActivity.class));
+    }
+
+    public void btn_goto_tests_onClick(View view) {
+        startActivity(new Intent(getApplicationContext(), TestsActivity.class));
+    }
+
+    public void btn_goto_games_onClick(View view) {
+        startActivity(new Intent(getApplicationContext(), GamesActivity.class));
     }
 }
 
