069bb2c7f0db4a54c24cc09467291fff77ce933a
Quinn Roundy <qjr3@students.uwf.edu>
2016-04-01 23:43:57 -0500

    MenuActivity MasterActivity , all activitys should inherit this now instead

--- app/src/main/java/com/example/hilla_000/firststep/MenuActivity.java
+++ app/src/main/java/com/example/hilla_000/firststep/MenuActivity.java
@@ -5,17 +5,18 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends MasterActivity {
+public class MenuActivity extends MasterActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_menu);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
 }
+
