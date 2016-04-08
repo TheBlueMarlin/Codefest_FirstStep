7e10c30b77257f98c2d17f3080c0d9d42530a6b9
OldxAge <jmr76@students.uwf.edu>
2016-04-07 23:33:30 -0600

    Title and Exercise Fragment Structure
    The structure for title and exercise is created. We need to tie the
    event handlers for clicking on them still.

---
+++ app/src/main/java/com/example/hilla_000/firststep/topTitleFragment.java
@@ -0,0 +1,17 @@
+package com.example.hilla_000.firststep;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class topTitleFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.top_title_fragment, container, false);
+        return view;
+    }
+}
