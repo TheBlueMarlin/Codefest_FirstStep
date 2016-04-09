50e0ee318264ee0abae64c2860151598634ae36b
OldxAge <jmr76@students.uwf.edu>
2016-04-08 20:12:45 -0600

    Load Screen Structure Created

--- app/src/main/java/com/example/hilla_000/firststep/ChoosePlayCat.java
+++ app/src/main/java/com/example/hilla_000/firststep/ChoosePlayCat.java
@@ -14,6 +14,8 @@ public class ChoosePlayCat extends SettingsMenuActivity {
         setContentView(R.layout.activity_choose_play_cat);
         openGame1();
         openGame2();
+        //openGame3();
+        //openGame4();
     }
     public void openGame1(){
         ImageButton button_choice = (ImageButton) findViewById(R.id.imageButton_play);
@@ -38,6 +40,38 @@ public class ChoosePlayCat extends SettingsMenuActivity {
                         @Override
                         public void onClick(View v) {
                             Intent intent = new Intent(getApplicationContext(), Game_Matching.class);
+                            startActivity(intent);
+                        }
+                    }
+            );
+        }
+    }
+
+    public void openGame3(){
+        ImageButton button_choice = (ImageButton) findViewById(R.id.imageButton3_play);
+        if (button_choice != null) {
+            button_choice.setOnClickListener(
+                    new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                             Intent intent = new Intent(getApplicationContext(), Game_Banana.class);
+                             //Uncomment and change destination.
+                            startActivity(intent);
+                        }
+                    }
+            );
+        }
+    }
+
+    public void openGame4(){
+        ImageButton button_choice = (ImageButton) findViewById(R.id.imageButton4_play);
+        if (button_choice != null) {
+            button_choice.setOnClickListener(
+                    new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent intent = new Intent(getApplicationContext(), Game_Matching.class);
+                            //Uncomment and change destination.
                             startActivity(intent);
                         }
                     }
